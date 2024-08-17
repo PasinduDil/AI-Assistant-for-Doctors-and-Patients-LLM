d7b5d71dfdb54ef92d3b401f975c2897fa5c55a5
PasinduDil <blackfoxxpasi@gmail.com>
2024-08-18 00:50:47 +0530

    last for today

--- app.py
+++ app.py
@@ -3,6 +3,51 @@ import streamlit as st
 from pathlib import Path
 import google.generativeai as genai
 
+from api_key import api_key
+
+#configure genai with api key
+genai.configure(api_key=api_key)
+
+#setup the model
+generation_config = {
+  "temperature": 0.4,
+  "top_p": 1,
+  "top_k": 32,
+  "max_output_tokens": 4096,
+}
+
+#apply safety Settings
+safety_settings=[
+    {
+        "category":"HARM_CATEGORY_HARASSMENT",
+        "threshold":"BLOCK_MRDIUM_AND_ABOVE"
+    },
+    {
+        "category":"HARM_CATEGORY_HATE_SPEECH",
+        "threshold":"BLOCK_MRDIUM_AND_ABOVE"
+    },
+    {
+        "category":"HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold":"BLOCK_MRDIUM_AND_ABOVE"
+    },
+    {
+        "category":"HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold":"BLOCK_MRDIUM_AND_ABOVE"
+    },
+
+
+
+]
+
+
+system_prompt=""""""
+#model configuration
+model=genai.GenerativeModel(model_name="gemini-pro-vision",
+                            generation_config=generation_config,
+                            safety_settings=safety_settings)
+
+
+
 # set the page configuration
 st.set_page_config(page_title="VitalImage Analytics",page_icon=":Robot:")
 
@@ -20,4 +65,21 @@ upoloaded_file=st.file_uploader("Upload as medical image for analysis",type=["pn
 submit_button=st.button("Genarate the Analysis")
 
 if submit_button:
-    pass
+    #process the uploaded image
+    image_data = uploaded_file.getvalue()
+
+    image_parts=[
+        {
+            "mime_type":"image/jpeg",
+            "data":image_data
+        },
+
+
+    ]
+
+    prompt_parts=[
+        image_parts[0],
+        "\n\nWhat is going on in this particular image?"
+
+
+    ]
