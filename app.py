2608e5a4c00f5cbea15da66d75404321c52255fe
PasinduDil <blackfoxxpasi@gmail.com>
2024-08-18 00:48:21 +0530

    config

--- app.py
+++ app.py
@@ -8,4 +8,16 @@ st.set_page_config(page_title="VitalImage Analytics",page_icon=":Robot:")
 
 #set the logo
 
-st.image("Image/Image.png",width=200)
+st.image("Image/Image.png",width=100)
+
+# Set the title
+
+st.title("Madicle Data Analytis")
+
+#set the sub header
+st.subheader("An application that can help users to identify medical image")
+upoloaded_file=st.file_uploader("Upload as medical image for analysis",type=["png","jpg","jpeg"])
+submit_button=st.button("Genarate the Analysis")
+
+if submit_button:
+    pass
